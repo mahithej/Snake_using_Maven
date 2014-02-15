811a5fd1976c92a98a873a1140146dc889e675cc
nikitozeg <tazg@ya.ru>
2014-02-15 19:00:23 +0400

    added Javadocs

--- src/main/java/ru/ifmo/nikita/Snake.java
+++ src/main/java/ru/ifmo/nikita/Snake.java
@@ -2,28 +2,27 @@ package ru.ifmo.nikita;
 
 import javax.swing.*;
 import javax.swing.JFrame;
-import java.awt.*;
 
-
+/**
+ * Window creating
+ */
 public class Snake extends JFrame {
-
     public Snake() {
-
         add(new Board());
-
-
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(500, 500);
         setLocationRelativeTo(null);
         setTitle("Snake");
-
         setResizable(false);
         setVisible(true);
 
     }
 
+    /**
+     * Main method.
+     * @param args  args
+     */
     public static void main(String[] args) {
         new Snake();
-
     }
 }
