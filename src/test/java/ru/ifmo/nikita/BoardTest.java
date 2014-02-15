98baf2bc91c3ba57acc44dcde422c9224e7d87bb
nikitozeg <tazg@ya.ru>
2014-02-15 18:30:21 +0400

    Added test class

---
+++ src/test/java/ru/ifmo/nikita/BoardTest.java
@@ -0,0 +1,28 @@
+package ru.ifmo.nikita;
+import org.junit.Ignore;
+import org.junit.Test;
+import static junit.framework.Assert.assertTrue;
+
+
+public class BoardTest extends Board {
+    @Ignore  ("not ready yet")
+    @ Test  public void  jj() {
+        Board boardIgnoreObj = new Board();
+        boardIgnoreObj.appleEscaping();
+    }
+
+    @Test
+        public void testCheckApple() {
+        Board boardObj = new Board();
+        x[0]=50;
+        y[0]=50;
+
+        boardObj.checkApple(70,70);
+        assertTrue(isNeedSearchAppleZone);
+
+    }
+
+
+
+
+}
