883815c699a91479c324640b6fa70cc22fb390b9
nikitozeg <tazg@ya.ru>
2014-02-23 16:53:27 +0400

    Unit test added new test

--- src/test/java/ru/ifmo/nikita/BoardTest.java
+++ src/test/java/ru/ifmo/nikita/BoardTest.java
@@ -1,28 +1,39 @@
 package ru.ifmo.nikita;
+
 import org.junit.Ignore;
 import org.junit.Test;
+
 import static junit.framework.Assert.assertTrue;
 
 
 public class BoardTest extends Board {
-    @Ignore  ("not ready yet")
-    @ Test  public void  jj() {
+    @Ignore("not ready yet")
+    @Test
+    public void jj() {
         Board boardIgnoreObj = new Board();
         boardIgnoreObj.appleEscaping();
     }
 
     @Test
-        public void testCheckApple() {
+    public void testCheckApple() {
         Board boardObj = new Board();
-        x[0]=50;
-        y[0]=50;
-
-        boardObj.checkApple(70,70);
-        assertTrue(isNeedSearchAppleZone);             //9
-
+        x[0] = 50;
+        y[0] = 50;
+        boardObj.checkApple(70, 70);
+        assertTrue(isNeedSearchAppleZone);
     }
 
-
-
+    @Test
+    public void test() {
+        checkApple(100, 100);
+        for (int i = 0; i < 16; i++) {
+            assertTrue("contains", appleSearchZoneXYList.contains(appleSearchZoneXYList.get((int) (Math.random() * 16))));
+        }
+    }
 
 }
+
+
+
+
+
