40daea631d6e367df9f911933ad1971d37d4ef72
nikitozeg <tazg@ya.ru>
2014-03-24 20:53:27 +0400

    ControlPanel and MouseListener moved to itself classes +object renamed

--- src/main/java/ru/ifmo/snake/Board.java
+++ src/main/java/ru/ifmo/snake/Board.java
@@ -43,10 +43,10 @@ class Board extends JPanel implements ActionListener, Runnable {
 
     public Board() {
         addKeyListener(new ActionListener());
-        ImageIcon _dot = new ImageIcon(this.getClass().getResource("/dot.png"));
-        body = _dot.getImage();
-        ImageIcon _apple = new ImageIcon(this.getClass().getResource("/apple.png"));
-        appleImg = _apple.getImage();
+        ImageIcon dot = new ImageIcon(this.getClass().getResource("/dot.png"));
+        body = dot.getImage();
+        ImageIcon apple = new ImageIcon(this.getClass().getResource("/apple.png"));
+        appleImg = apple.getImage();
         initGame();
         setFocusable(true);
         setBackground(Color.LIGHT_GRAY);
@@ -289,43 +289,3 @@ class Board extends JPanel implements ActionListener, Runnable {
 }
 
 
-class MouseListener extends MouseAdapter {
-    public void mouseClicked(MouseEvent event) {
-        // System.exit(0);
-        new Board();
-    }
-}
-
-/**
- * The bottom panel with 2 buttons on it.
- */
-class ControlPanel extends JPanel {
-    JButton butRestart = new JButton("Restart");
-    JButton butExit = new JButton("Exit");
-    JLabel TFObj = new JLabel();
-
-    /**
-     * Setting buttons on Panel.
-     */
-    ControlPanel() {
-        setBounds(50, 550, 500, 70);
-        setLayout(null);
-        setBackground(Color.LIGHT_GRAY);
-        butRestart.setBounds(100, 10, 100, 50);
-        add(butRestart);
-        butExit.setBounds(350, 10, 100, 50);
-        add(butExit);
-        butExit.addMouseListener(new MouseListener());
-        butRestart.addMouseListener(new MouseListener());
-    }
-
-    /**
-     * Trying to set a Score on panel.
-     */
-    public void setLabel() {
-        TFObj.setText("sssssssssssssssssssssS");
-        TFObj.setBounds(0, 0, 50, 50);
-        add(TFObj);
-    }
-
-}
